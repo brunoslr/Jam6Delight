8b3e72eef643e47805c015fa971c15cc7e780ef6
unknown <bsr1615@GDD-39.main.ad.rit.edu>
2015-04-05 17:54:48 -0400

    Its raining woman

--- Assets/Scripts/ResourceSpawner.cs
+++ Assets/Scripts/ResourceSpawner.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+ 
 public class ResourceSpawner : MonoBehaviour {
 
     public GameObject resourcePrefab;
     private Transform resourceContainer;
+	public Sprite[] womanSprites;
 
 	void Start () {
         resourceContainer = GameObject.Find("Resources").transform;
+
 	}
-	
+
+
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
@@ -17,6 +20,8 @@ public class ResourceSpawner : MonoBehaviour {
             pos.z = 0;
             var obj = (GameObject)Instantiate(resourcePrefab, pos, Quaternion.identity);
             obj.transform.parent = resourceContainer;
+	//		obj.AddComponent<SpriteRenderer>();
+			obj.GetComponentInChildren<SpriteRenderer>().sprite = womanSprites[Random.Range(0,womanSprites.Length)];
         }	
 	}
 }
